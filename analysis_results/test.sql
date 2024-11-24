f674e614e66d0f9527da8e0ceeffc3d36a162920
victorvasss <95033546+victorvasss@users.noreply.github.com>
2024-11-24 22:06:10 +0300

    analyzer now scans 4 queries and put results to json

--- analysis_results/test.sql
+++
@@ -1,6 +0,0 @@
-grade: Executed
-recommendations: []
-score: 3
-checks: {'null_check': True, 'duplicates_check': True, 'completeness_check': True}
-execution_time: 0.0
-explain: [('Seq Scan on products  (cost=0.00..12.70 rows=270 width=274) (actual time=0.011..0.013 rows=5 loops=1)',), ('Planning Time: 0.050 ms',), ('Execution Time: 0.027 ms',)]
