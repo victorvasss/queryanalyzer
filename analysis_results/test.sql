dc61e85b1d283624dbd460713bfb0cafba5dc17c
RiftFate <97227751+RiftFate@users.noreply.github.com>
2024-11-27 17:39:34 +0300

    Stash resolve

--- analysis_results/test.sql
+++ analysis_results/test.sql
@@ -3,4 +3,4 @@ recommendations: []
 score: 3
 checks: {'null_check': True, 'duplicates_check': True, 'completeness_check': True}
 execution_time: 0.0
-explain: [('Seq Scan on products  (cost=0.00..12.70 rows=270 width=274) (actual time=0.011..0.013 rows=5 loops=1)',), ('Planning Time: 0.050 ms',), ('Execution Time: 0.027 ms',)]
+explain: [('Seq Scan on products  (cost=0.00..12.70 rows=270 width=274) (actual time=0.015..0.016 rows=5 loops=1)',), ('Planning Time: 0.054 ms',), ('Execution Time: 0.166 ms',)]
