4f7eab383a310650031ae756475d6e5a7b2df6bf
RiftFate <97227751+RiftFate@users.noreply.github.com>
2025-01-16 19:06:50 +0300

    [*] results: modal scroll

--- students_sql/2025-01-16_18-26-16/test2.sql
+++
@@ -1,5 +0,0 @@
-SELECT name FROM Products WHERE price > 100;
-;
-SELECT name FROM Products WHERE category_id IS NOT NULL ORDER BY price DESC LIMIT 3;asdcdc
-SELECT category_id, AVG(price) AS avg_price FROM Products GROUP BY category_id;
-asdcasc SELECT category_id, AVG(price) AS avg_price FROM Products GROUP BY category_id;
